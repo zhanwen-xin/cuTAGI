78eb389f3694b4abaad60cc9bbbdd8086eda03a3
zhanwenxin22 <zhanwen.w.xin@gmail.com>
2024-09-19 15:18:41 -0400

    Add LSTM functions

--- examples/data_loader.py
+++ examples/data_loader.py
@@ -301,7 +301,9 @@ class TimeSeriesDataloader:
         x_std: Optional[np.ndarray] = None,
         ts_idx: Optional[int] = None,
         time_covariates: Optional[str] = None,
-        keep_last_time_cov: Optional[bool] = False,
+        add_anomaly: Optional[bool] = False,
+        anomaly_magnitude: Optional[float] = 0.1,
+        anomaly_start: Optional[int] = 0,
     ) -> None:
         self.x_file = x_file
         self.date_time_file = date_time_file
@@ -312,9 +314,11 @@ class TimeSeriesDataloader:
         self.stride = stride
         self.x_mean = x_mean
         self.x_std = x_std
-        self.ts_idx = ts_idx  # add time series index when data having multiple ts
-        self.time_covariates = time_covariates  # for adding time covariates
-        self.keep_last_time_cov = keep_last_time_cov
+        self.ts_idx = ts_idx # add time series index when data having multiple ts
+        self.time_covariates = time_covariates # for adding time covariates
+        self.add_anomaly = add_anomaly
+        self.anomaly_magnitude = anomaly_magnitude
+        self.anomaly_start = anomaly_start
         self.dataset = self.process_data()
 
     def load_data_from_csv(self, data_file: str) -> pd.DataFrame:
@@ -340,8 +344,8 @@ class TimeSeriesDataloader:
             np.random.shuffle(indices)
 
         for start_idx in range(0, num_data, batch_size):
-            # if start_idx + batch_size > num_data:
-            #     continue
+            if start_idx + batch_size > num_data:
+                continue
             end_idx = min(start_idx + batch_size, num_data)
             idx = indices[start_idx:end_idx]
             yield input_data[idx].flatten(), output_data[idx].flatten()
@@ -353,36 +357,42 @@ class TimeSeriesDataloader:
 
         # Load data
         x = self.load_data_from_csv(self.x_file)
+
+        # Add anomaly
+        if self.add_anomaly:
+            for i in range(self.anomaly_start, x.shape[0]):
+                x[i] += self.anomaly_magnitude * (i - self.anomaly_start)
+
+        # Remove all the columns except the first one, no explanatory variable is used
+        x = x[:, 0].reshape(-1, 1)
+
         if self.ts_idx is not None:
-            x = x[:, self.ts_idx : self.ts_idx + 1]  # choose time series column
+            x = x[:,self.ts_idx:self.ts_idx+1]   # choose time series column
         date_time = self.load_data_from_csv(self.date_time_file)
 
         # Add time covariates
         if self.time_covariates is not None:
-            date_time = np.array(date_time, dtype="datetime64")
+            date_time = np.array(date_time, dtype='datetime64')
             for time_cov in self.time_covariates:
-                if time_cov == "hour_of_day":
-                    hour_of_day = date_time.astype("datetime64[h]").astype(int) % 24
-                    x = np.concatenate((x, hour_of_day), axis=1)
-                elif time_cov == "day_of_week":
-                    day_of_week = date_time.astype("datetime64[D]").astype(int) % 7
-                    x = np.concatenate((x, day_of_week), axis=1)
-                elif time_cov == "week_of_year":
-                    week_of_year = (
-                        date_time.astype("datetime64[W]").astype(int) % 52 + 1
-                    )
-                    x = np.concatenate((x, week_of_year), axis=1)
-                elif time_cov == "month_of_year":
-                    month_of_year = (
-                        date_time.astype("datetime64[M]").astype(int) % 12 + 1
-                    )
-                    x = np.concatenate((x, month_of_year), axis=1)
-                elif time_cov == "quarter_of_year":
-                    month_of_year = (
-                        date_time.astype("datetime64[M]").astype(int) % 12 + 1
-                    )
+                if time_cov == 'hour_of_day':
+                    hour_of_day = date_time.astype('datetime64[h]').astype(int) % 24
+                    x = np.concatenate((x,hour_of_day),axis=1)
+                elif time_cov == 'day_of_week':
+                    day_of_week = date_time.astype('datetime64[D]').astype(int) % 7
+                    x = np.concatenate((x,day_of_week),axis=1)
+                elif time_cov == 'week_of_year':
+                    week_of_year = date_time.astype('datetime64[W]').astype(int) % 52 + 1
+                    x = np.concatenate((x,week_of_year),axis=1)
+                elif time_cov == 'month_of_year':
+                    month_of_year = date_time.astype('datetime64[M]').astype(int) % 12 + 1
+                    x = np.concatenate((x,month_of_year),axis=1)
+                elif time_cov == 'quarter_of_year':
+                    month_of_year = date_time.astype('datetime64[M]').astype(int) % 12 + 1
                     quarter_of_year = (month_of_year - 1) // 3 + 1
-                    x = np.concatenate((x, quarter_of_year), axis=1)
+                    x = np.concatenate((x,quarter_of_year),axis=1)
+                elif time_cov == 'day_of_year':
+                    day_of_year = date_time.astype('datetime64[D]').astype(int) % 365
+                    x = np.concatenate((x,day_of_year),axis=1)
 
         # Normalizer
         if self.x_mean is None and self.x_std is None:
@@ -399,9 +409,156 @@ class TimeSeriesDataloader:
             stride=self.stride,
         )
 
-        # remove time covariates, only keep the time cov at the last time step
-        if self.keep_last_time_cov:
-            x_rolled = self.remove_time_cov(x_rolled)
+        # Dataloader
+        dataset = {}
+        dataset["value"] = (x_rolled, y_rolled)
+
+        # NOTE: Datetime is saved for the visualization purpose
+        dataset["date_time"] = [np.datetime64(date) for date in np.squeeze(date_time)]
+
+        return dataset
+
+    def create_data_loader(self, batch_size: int, shuffle: bool = True):
+        return self.batch_generator(*self.dataset["value"], batch_size, shuffle)
+
+
+class SyntheticTimeSeriesDataloader:
+    """Data loader for time series"""
+
+    def __init__(
+        self,
+        x_file: str,
+        date_time_file: str,
+        output_col: np.ndarray,
+        input_seq_len: int,
+        output_seq_len: int,
+        num_features: int,
+        stride: int,
+        x_mean: Optional[np.ndarray] = None,
+        x_std: Optional[np.ndarray] = None,
+        ts_idx: Optional[int] = None,
+        select_column: Optional[int] = 0,
+        add_anomaly: Optional[bool] = False,
+        anomaly_magnitude: Optional[float] = 0.1,
+        anomaly_start: Optional[int] = 0,
+        time_covariates: Optional[str] = None,
+    ) -> None:
+        self.x_file = x_file
+        self.date_time_file = date_time_file
+        self.output_col = output_col
+        self.input_seq_len = input_seq_len
+        self.output_seq_len = output_seq_len
+        self.num_features = num_features
+        self.stride = stride
+        self.x_mean = x_mean
+        self.x_std = x_std
+        self.ts_idx = ts_idx # add time series index when data having multiple ts
+        self.select_column = select_column
+        self.add_anomaly = add_anomaly
+        self.anomaly_magnitude = anomaly_magnitude
+        self.anomaly_start = anomaly_start
+        self.time_covariates = time_covariates # for adding time covariates
+        self.dataset = self.process_data()
+
+    def load_data_from_csv(self, data_file: str, skip_row = 0, select_column = None) -> pd.DataFrame:
+        """Load data from csv file"""
+
+        data = pd.read_csv(data_file, skiprows=skip_row, delimiter=",", header=None)
+
+        # if select_column is not given by the user, reshape the data to (-1, 1)
+        if select_column is None:
+            return data.values
+        else:
+            # if select_column is given by the user, return the selected column
+            return data.values[:, select_column].reshape(-1, 1)
+
+    @staticmethod
+    def batch_generator(
+        input_data: np.ndarray,
+        output_data: np.ndarray,
+        batch_size: int,
+        shuffle: bool = True,
+    ) -> Generator[Tuple[np.ndarray, ...], None, None]:
+        """
+        Generator function to yield batches of data.
+        """
+        num_data = input_data.shape[0]
+        indices = np.arange(num_data)
+        if shuffle:
+            np.random.shuffle(indices)
+
+        for start_idx in range(0, num_data, batch_size):
+            if start_idx + batch_size > num_data:
+                continue
+            end_idx = min(start_idx + batch_size, num_data)
+            idx = indices[start_idx:end_idx]
+            yield input_data[idx].flatten(), output_data[idx].flatten()
+
+    def process_data(self) -> dict:
+        """Process time series"""
+        # Initialization
+        utils = Utils()
+
+        # Load data
+        x = self.load_data_from_csv(self.x_file, select_column=self.select_column)
+
+        # Add anomaly
+        if self.add_anomaly:
+            # determine if self.anomaly_magnitude is a single float value or a list of two float values
+            if isinstance(self.anomaly_magnitude, float):
+                for i in range(self.anomaly_start, x.shape[0]):
+                    x[i] += self.anomaly_magnitude * (i - self.anomaly_start)
+            else:
+                for i in range(self.anomaly_start, x.shape[0]):
+                    x[i] += self.anomaly_magnitude[0] * (i - self.anomaly_start)
+                for i in range(self.anomaly_start + int((x.shape[0]-26-65)/2), x.shape[0]):
+                    x[i] += self.anomaly_magnitude[1] * (i - self.anomaly_start - int((x.shape[0]-26-65)/2))
+
+        # # Remove all the columns except the first one, no explanatory variable is used
+        # x = x[:, 0].reshape(-1, 1)
+
+        if self.ts_idx is not None:
+            x = x[:,self.ts_idx:self.ts_idx+1]   # choose time series column
+        date_time = self.load_data_from_csv(self.date_time_file, skip_row=1)
+
+        # Add time covariates
+        if self.time_covariates is not None:
+            date_time = np.array(date_time, dtype='datetime64')
+            for time_cov in self.time_covariates:
+                if time_cov == 'hour_of_day':
+                    hour_of_day = date_time.astype('datetime64[h]').astype(int) % 24
+                    x = np.concatenate((x,hour_of_day),axis=1)
+                elif time_cov == 'day_of_week':
+                    day_of_week = date_time.astype('datetime64[D]').astype(int) % 7
+                    x = np.concatenate((x,day_of_week),axis=1)
+                elif time_cov == 'week_of_year':
+                    week_of_year = date_time.astype('datetime64[W]').astype(int) % 52 + 1
+                    x = np.concatenate((x,week_of_year),axis=1)
+                elif time_cov == 'month_of_year':
+                    month_of_year = date_time.astype('datetime64[M]').astype(int) % 12 + 1
+                    x = np.concatenate((x,month_of_year),axis=1)
+                elif time_cov == 'quarter_of_year':
+                    month_of_year = date_time.astype('datetime64[M]').astype(int) % 12 + 1
+                    quarter_of_year = (month_of_year - 1) // 3 + 1
+                    x = np.concatenate((x,quarter_of_year),axis=1)
+                elif time_cov == 'day_of_year':
+                    day_of_year = date_time.astype('datetime64[D]').astype(int) % 365
+                    x = np.concatenate((x,day_of_year),axis=1)
+
+        # Normalizer
+        if self.x_mean is None and self.x_std is None:
+            self.x_mean, self.x_std = Normalizer.compute_mean_std(x)
+        x = Normalizer.standardize(data=x, mu=self.x_mean, std=self.x_std)
+
+        # Create rolling windows
+        x_rolled, y_rolled = utils.create_rolling_window(
+            data=x,
+            output_col=self.output_col,
+            input_seq_len=self.input_seq_len,
+            output_seq_len=self.output_seq_len,
+            num_features=self.num_features,
+            stride=self.stride,
+        )
 
         # Dataloader
         dataset = {}
@@ -412,106 +569,5 @@ class TimeSeriesDataloader:
 
         return dataset
 
-    def remove_time_cov(self, x):
-        x_new = np.zeros((len(x), self.input_seq_len + self.num_features - 1), dtype=np.float32)
-        for i in range(0,len(x)):
-            x_ = x[i]
-            keep_idx = np.arange(0, len(x_), self.num_features)
-            x_new[i] = np.concatenate((x_[keep_idx],x_[-self.num_features+1:]))
-        return x_new
-
     def create_data_loader(self, batch_size: int, shuffle: bool = True):
         return self.batch_generator(*self.dataset["value"], batch_size, shuffle)
-
-
-# class TimeSeriesDataloader:
-#     """Data loader for time series"""
-
-#     def __init__(
-#         self,
-#         x_file: str,
-#         date_time_file: str,
-#         output_col: np.ndarray,
-#         input_seq_len: int,
-#         output_seq_len: int,
-#         num_features: int,
-#         stride: int,
-#         x_mean: Optional[np.ndarray] = None,
-#         x_std: Optional[np.ndarray] = None,
-#     ) -> None:
-#         self.x_file = x_file
-#         self.date_time_file = date_time_file
-#         self.output_col = output_col
-#         self.input_seq_len = input_seq_len
-#         self.output_seq_len = output_seq_len
-#         self.num_features = num_features
-#         self.stride = stride
-#         self.x_mean = x_mean
-#         self.x_std = x_std
-
-#         self.dataset = self.process_data()
-
-#     def load_data_from_csv(self, data_file: str) -> pd.DataFrame:
-#         """Load data from csv file"""
-
-#         data = pd.read_csv(data_file, skiprows=1, delimiter=",", header=None)
-
-#         return data.values
-
-#     @staticmethod
-#     def batch_generator(
-#         input_data: np.ndarray,
-#         output_data: np.ndarray,
-#         batch_size: int,
-#         shuffle: bool = True,
-#     ) -> Generator[Tuple[np.ndarray, ...], None, None]:
-#         """
-#         Generator function to yield batches of data.
-#         """
-#         num_data = input_data.shape[0]
-#         indices = np.arange(num_data)
-#         if shuffle:
-#             np.random.shuffle(indices)
-
-#         for start_idx in range(0, num_data, batch_size):
-#             if start_idx + batch_size > num_data:
-#                 continue
-#             end_idx = min(start_idx + batch_size, num_data)
-#             idx = indices[start_idx:end_idx]
-#             yield input_data[idx].flatten(), output_data[idx].flatten()
-
-#     def process_data(self) -> dict:
-#         """Process time series"""
-#         # Initialization
-#         utils = Utils()
-
-#         # Load data
-#         x = self.load_data_from_csv(self.x_file)
-#         date_time = self.load_data_from_csv(self.date_time_file)
-
-#         # Normalizer
-#         if self.x_mean is None and self.x_std is None:
-#             self.x_mean, self.x_std = Normalizer.compute_mean_std(x)
-#         x = Normalizer.standardize(data=x, mu=self.x_mean, std=self.x_std)
-
-#         # Create rolling windows
-#         x_rolled, y_rolled = utils.create_rolling_window(
-#             data=x,
-#             output_col=self.output_col,
-#             input_seq_len=self.input_seq_len,
-#             output_seq_len=self.output_seq_len,
-#             num_features=self.num_features,
-#             stride=self.stride,
-#         )
-
-#         # Dataloader
-#         dataset = {}
-#         dataset["value"] = (x_rolled, y_rolled)
-
-#         # NOTE: Datetime is saved for the visualization purpose
-#         dataset["date_time"] = [np.datetime64(date) for date in np.squeeze(date_time)]
-
-#         return dataset
-
-#     def create_data_loader(self, batch_size: int, shuffle: bool = True):
-#         return self.batch_generator(*self.dataset["value"], batch_size, shuffle)
